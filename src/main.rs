f5a3fe64df4c676ec3c44ca9c31acd376c10878c
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2020-09-17 20:24:01 +0100

    segmentation of gui and game logic

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,71 @@ use sdl2::rect::{Rect, Point};
 use sdl2::render::WindowCanvas;
 use std::mem::swap;
 
-struct Board {
-    turns: i64,
-    tiles: [[bool; 20]; 10]
+enum GuiState {
+    Menu,
+    Game
 }
 
-impl Board {
+type Tile = Vec<Point>;
+
+struct AppState {
+    gui_state: GuiState,
+    game_state: GameState,
+}
+
+struct GameState {
+    tiles: [[bool; 20]; 10],
+    turns: i64,
+    active: Option<Tile>,
+}
+
+impl AppState {
+    fn draw(&self, canvas: &mut WindowCanvas) -> Result<(), String> {
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        self.game_state.draw(canvas)?;
+
+        match self.gui_state {
+            GuiState::Menu => canvas.set_draw_color(Color::RGB(255, 0, 0)),
+            GuiState::Game => canvas.set_draw_color(Color::RGB(0, 255, 0)),
+        }
+        canvas.fill_rect(Rect::new(500, 100, 200, 200))?;
+
+        canvas.present();
+
+        Ok(())
+    }
+
+    fn handle(&mut self, event: Event) -> bool {
+        match event {
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => false,
+            Event::KeyDown { keycode: Some(Keycode::Space),
+                ..
+            } => {
+                match &self.gui_state {
+                    GuiState::Menu => self.gui_state = GuiState::Game,
+                    GuiState::Game => self.gui_state = GuiState::Menu,
+                }
+                true
+            }
+            _ => self.game_state.handle(event)
+        }
+    }
+
+    fn update(&mut self) {
+        match self.gui_state {
+            GuiState::Menu => (),
+            GuiState::Game => self.game_state.update(),
+        }
+    }
+}
+
+impl GameState {
     fn draw(&self, canvas: &mut WindowCanvas) -> Result<(), String> {
         canvas.set_draw_color(Color::RGB(255, 255, 0));
         for i in 0..self.tiles.len() {
@@ -37,6 +96,36 @@ impl Board {
         Ok(())
     }
 
+    fn handle(&mut self, event: Event) -> bool {
+        match event {
+            Event::MouseMotion {
+                timestamp, window_id, which, mousestate, x, y, xrel, yrel
+            } => {
+                if mousestate.left() {
+                    let x = (x / 40) as usize;
+                    let y = (y / 40) as usize;
+                    if x < 10 {
+                        if !self.tiles[x][y] {
+                            self.tiles[x][y] = true;
+                        }
+                    }
+                }
+            }
+            Event::MouseButtonDown {
+                timestamp, window_id, which, mouse_btn: MouseButton::Left, clicks, x, y
+            } => {
+                let x = (x / 40) as usize;
+                let y = (y / 40) as usize;
+                if x < 10 {
+                    let tile = &mut self.tiles[x][y];
+                    *tile = !*tile;
+                }
+            }
+            _ => ()
+        }
+        true
+    }
+
     fn update(&mut self) {
         if self.turns % 30 == 0 {
             for i in 0..self.tiles.len() {
@@ -47,19 +136,21 @@ impl Board {
                     }
                 }
             }
-        }
 
-        for j in 0..self.tiles[0].len() {
-            let mut allGood = true;
-            for i in 0..self.tiles.len() {
-                if !self.tiles[i][j] {
-                    allGood = false;
-                    break;
+            for j in 0..self.tiles[0].len() {
+                if self.tiles.iter().map(|row| row[j]).all(|v| v) {
+                    for i in 0..self.tiles.len() {
+                        self.tiles[i][j] = false;
+                    }
                 }
             }
-            if allGood {
-                for i in 0..self.tiles.len() {
-                    self.tiles[i][j] = false;
+
+            for j in (1..self.tiles[0].len()).rev() {
+                if self.tiles.iter().map(|row| row[j]).all(|v| !v) {
+                    for i in 0..self.tiles.len() {
+                        self.tiles[i][j] = self.tiles[i][j - 1];
+                        self.tiles[i][j - 1] = false;
+                    }
                 }
             }
         }
@@ -87,8 +178,9 @@ fn main() -> Result<(), String>{
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
-    // canvas.set_scale(1, 1.0)?;
+    let mut canvas = window.into_canvas()
+        .build()
+        .unwrap();
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
@@ -97,44 +189,26 @@ fn main() -> Result<(), String>{
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut frame = 0;
 
-    let mut state = Board { tiles: [[false; 20]; 10], turns: 0 };
+    let mut app_state = AppState {
+        gui_state: GuiState::Menu,
+        game_state: GameState {
+            tiles: [[false; 20]; 10],
+            turns: 0,
+            active: None
+        }
+    };
 
     'running: loop {
         frame += 1;
 
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                Event::MouseButtonDown {
-                    timestamp, window_id, which, mouse_btn, clicks, x, y
-                } => {
-                    match mouse_btn {
-                        MouseButton::Left => {
-                            let x = (x / 40) as usize;
-                            let y = (y / 40) as usize;
-                            if x < 10 {
-                                let tile = &mut state.tiles[x][y];
-                                *tile = !*tile;
-                            }
-                        }
-                        _ => ()
-                    }
-                }
-                _ => ()
+            if !app_state.handle(event) {
+                break 'running
             }
         }
 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-
-        state.update();
-        state.draw(&mut canvas)?;
-
-        canvas.present();
+        app_state.update();
+        app_state.draw(&mut canvas)?;
 
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
